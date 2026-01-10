42b05c097717b31e647c9d51bb0c7231b70dfcbf
Ersomsharma <som2000.sharma@gmail.com>
2026-01-10 19:34:51 +0530

    this is my MajorProject

---
+++ accounts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active', 'is_staff')
+    list_display_links = ('email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ('is_active', 'is_staff')
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
